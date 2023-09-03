30db40497070122ed8a57a89e2e19d0d4a9a703e
IacobOliver <olialexander08@gmail.com>
2023-09-03 16:01:02 +0300

    finished the filter and started the authentication folder

---
+++ Backend/src/main/java/com/codecooll/RestaurantMania/authentication/AuthenticationService.java
@@ -0,0 +1,18 @@
+package com.codecooll.RestaurantMania.authentication;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        return null;
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
+    }
+}
--- Backend/src/main/java/com/codecooll/RestaurantMania/security/ApplicationConfig.java
+++ Backend/src/main/java/com/codecooll/RestaurantMania/security/ApplicationConfig.java
@@ -5,9 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,6 +25,24 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService (){
         return username -> accountRepository.findAccountByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider (){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncored());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncored() {
+        return new BCryptPasswordEncoder();
     }
 
 }
