c4dc43d711e2971095ca12e8ecaae741b28ffe54
IacobOliver <olialexander08@gmail.com>
2023-09-14 09:47:24 +0300

    fixed cors

--- Backend/src/main/java/com/codecooll/RestaurantMania/security/SecurityConfig.java
+++ Backend/src/main/java/com/codecooll/RestaurantMania/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -25,6 +26,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
          return http
+                 .cors(Customizer.withDefaults())
                .csrf( csrf -> csrf.disable())
                .authorizeHttpRequests(auth -> auth.requestMatchers("api/v1/auth/**", "restaurant/**")
                                                     .permitAll()
