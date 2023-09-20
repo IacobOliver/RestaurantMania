59ca0e349208657b63fde432f572c3afa7d5ff9b
Valentin1289 <valibaiacof1289@gmail.com>
2023-09-20 17:38:06 +0300

    refactor update restaurant

--- Backend/src/main/java/com/codecooll/RestaurantMania/data/repository/RestaurantRepository.java
+++ Backend/src/main/java/com/codecooll/RestaurantMania/data/repository/RestaurantRepository.java
@@ -1,9 +1,11 @@
-package com.codecooll.RestaurantMania.restaurant.service.restaurantService;
+package com.codecooll.RestaurantMania.data.repository;
 
 import com.codecooll.RestaurantMania.restaurant.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,4 +24,21 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
             "FROM Restaurant r " +
             "WHERE r.id = :restaurant_id")
     Optional<Restaurant> findByIdWithoutMenu(long restaurant_id);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Restaurant r SET r.description = :newDescription WHERE r.id = :id")
+    void updateDescriptionById(Long id, String newDescription);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Restaurant r SET r.name = :newName WHERE r.id = :id")
+    void updateNameById(Long id, String newName);
+
+//    @Modifying
+//    @Transactional
+//    @Query("UPDATE Restaurant r SET r.image.image_url = :newImageUrl WHERE r.id = :id")
+//    void updateImageById(Long id, String newImageUrl);
+
+
 }
