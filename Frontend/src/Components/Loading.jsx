013e1a7245a7e06fa0b5dc1dbf027e974a7590cb
Oliver <olialexander08@gmail.com>
2023-09-26 17:35:28 +0300

    changed loading

--- Frontend/src/Components/Loading.jsx
+++ Frontend/src/Components/Loading.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 export default function Loading() {
   return (
-    <div className="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center ">
-      <span className="loading loading-ring w-60"></span> 
+    <div className="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center" >
+      <div className=" w-56 h-56 rounded-full flex items-center justify-center" style={{backgroundColor:"rgba(0,0,0,0.3)"}}>
+      {/* <span className="loading loading-ring w-60"></span>  */}
+      <img src="\public\images\loadingPot.gif"/>
+      </div>
     </div>
   );
 }
